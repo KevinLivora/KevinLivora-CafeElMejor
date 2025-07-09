ee6af88c0f22d62ae28bbe1473d1c46152ece70c
Kevin Livora <157827465+KevinLivora@users.noreply.github.com>
2025-07-09 17:26:13 -0300

    Update carousel to show single image

--- CafeElMejor/frontend/landing.js
+++ CafeElMejor/frontend/landing.js
@@ -9,7 +9,7 @@ punto.forEach( ( cadaPunto , i )=> {
         // Guardar la posición de ese PUNTO
         let posicion  = i
         // Calculando el espacio que debe DESPLAZARSE el GRANDE
-        let operacion = posicion * -50
+        let operacion = posicion * -100
 
         // MOVEMOS el grande
         grande.style.transform = `translateX(${ operacion }%)`
